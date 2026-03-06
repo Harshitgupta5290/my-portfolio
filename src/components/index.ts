22ee8471ea8d39bbab964be388f16d4ed7c073bd
Harshitgupta5290 <harshitgupta5290@gmail.com>
2026-03-06 23:33:37 +0530

    update

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+export { Navbar } from './navbar';
+export { Hero } from './hero';
+export { Experience } from './experience';
+export { Skills } from './skills';
+export { Projects } from './projects';
+export { Blog } from './blog';
+export { FAQSection } from './faq';
+export { Contact } from './contact';
+export { Section, SectionTitle, Container, FadeInUp } from './section-wrapper';
